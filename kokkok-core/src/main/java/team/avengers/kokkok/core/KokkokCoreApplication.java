b3a025f7165f117ebbbe6dbf0f1c0e6a1e3fd55a
YoungWukJeon <kdh10080@gmail.com>
2022-02-02 21:44:14 +0900

    initial commit

---
+++ kokkok-core/src/main/java/team/avengers/kokkok/core/KokkokCoreApplication.java
@@ -0,0 +1,13 @@
+package team.avengers.kokkok.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokkokCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokkokCoreApplication.class, args);
+    }
+
+}
---
+++ kokkok-core/src/test/java/team/avengers/kokkok/core/KokkokCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package team.avengers.kokkok.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokkokCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
