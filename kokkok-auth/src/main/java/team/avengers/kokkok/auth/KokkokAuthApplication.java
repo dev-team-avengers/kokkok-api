9b193a0bfe4c65ac30b4023c616c1a8112bcb03d
YoungWukJeon <kdh10080@gmail.com>
2022-02-05 17:17:30 +0900

    멀티폼 세팅

---
+++ kokkok-auth/src/main/java/team/avengers/kokkok/auth/KokkokAuthApplication.java
@@ -0,0 +1,13 @@
+package team.avengers.kokkok.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KokkokAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KokkokAuthApplication.class, args);
+    }
+
+}
---
+++ kokkok-auth/src/test/java/team/avengers/kokkok/auth/KokkokAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package team.avengers.kokkok.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KokkokAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
