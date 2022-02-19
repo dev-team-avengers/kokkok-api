d260b4ffbce95f5bbbd84255d44d50b1bc50d7b0
YoungWukJeon <kdh10080@gmail.com>
2022-02-19 17:38:50 +0900

    user 테이블 설계 중

--- kokkok-auth/src/main/java/team/avengers/kokkok/auth/entity/User.java
+++ kokkok-auth/src/main/java/team/avengers/kokkok/auth/entity/User.java
@@ -1,11 +1,11 @@
 package team.avengers.kokkok.auth.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 
 public record User(
-        @Id
-        Long id,
-        String firstName,
-        String lastName
+        @Id Long seq,
+        @Column String id
+
 ) {
 }
--- kokkok-auth/src/main/resources/schema.sql
+++ kokkok-auth/src/main/resources/schema.sql
@@ -1,1 +1,17 @@
-CREATE TABLE `user` (id BIGINT PRIMARY KEY AUTO_INCREMENT, first_name VARCHAR(255), last_name VARCHAR(255));
+CREATE TABLE `user` (
+    seq BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id VARCHAR(30) NOT NULL UNIQUE,
+    `password` VARCHAR(128),
+    nickname VARCHAR(30) NOT NULL UNIQUE,
+    is_verified_email CHAR(1) NOT NULL DEFAULT 'N',
+    email VARCHAR(128),
+    is_verified_phone_number CHAR(1) NOT NULL DEFAULT 'N',
+    phone_number VARCHAR(128),
+    join_type TINYINT(1),
+    user_type TINYINT(1) NOT NULL,
+    create_date DATETIME NOT NULL,
+    modify_date DATETIME NOT NULL,
+    last_login_at DATETIME NOT NULL,
+    join_date DATETIME,
+    leave_date DATETIME
+)
