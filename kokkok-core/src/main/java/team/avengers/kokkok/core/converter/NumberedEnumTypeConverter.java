4848345f447ac38029c7e23ac37195bcb0b9cf26
YoungWukJeon <kdh10080@gmail.com>
2022-03-20 14:50:52 +0900

    primitive 타입 제거

--- kokkok-core/src/main/java/team/avengers/kokkok/core/converter/NumberedEnumTypeConverter.java
+++ kokkok-core/src/main/java/team/avengers/kokkok/core/converter/NumberedEnumTypeConverter.java
@@ -1,8 +1,12 @@
 package team.avengers.kokkok.core.converter;
 
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.WritingConverter;
 
 @WritingConverter
-public class NumberedEnumTypeConverter {
-
+public class NumberedEnumTypeConverter<S, T> implements Converter<S, T> {
+    @Override
+    public T convert(S source) {
+        return null;
+    }
 }
